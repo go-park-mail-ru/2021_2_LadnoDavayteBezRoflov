3c901b48b71093487ded0301e894db29fcb7aaab
Антон Чумаков <anton@MacBook-Air-Anton.local>
2021-11-13 10:38:47 +0300

    LDBR-2.30: Исправить удаление доски

--- app/models/board.go
+++ app/models/board.go
@@ -6,5 +6,5 @@ type Board struct {
 	Title       string     `json:"board_name" faker:"word" gorm:"not null;index"`
 	Description string     `json:"description" faker:"sentence"`
 	CardLists   []CardList `json:"card_lists" faker:"-" gorm:"foreignKey:BID;constraint:OnDelete:CASCADE;"`
-	Cards       []Card     `json:"-" faker:"-" gorm:"foreignKey:BID;"`
+	Cards       []Card     `json:"-" faker:"-" gorm:"foreignKey:BID;constraint:OnDelete:CASCADE;"`
 }
