d3113cfac00308ae05ca8b3b576fec2343f71ba4
Антон Чумаков <anton@MacBook-Air-Anton.local>
2021-11-17 23:40:35 +0300

    LDBR-3.16: Удалить закомментированную строчку

--- app/models/card.go
+++ app/models/card.go
@@ -1,14 +1,13 @@
 package models
 
 type Card struct {
-	CID                uint   `json:"cid" gorm:"primaryKey"`
-	BID                uint   `json:"bid" gorm:"not null;index"`
-	CLID               uint   `json:"clid" gorm:"not null;index"`
-	PositionOnCardList uint   `json:"pos" faker:"-"`
-	Title              string `json:"card_name" faker:"word" gorm:"not null;index"`
-	Description        string `json:"description" faker:"sentence"`
-	// Deadline           time.Time `json:"-" faker:"-"`
-	DeadlineChecked bool      `json:"deadline_check" faker:"-"`
-	Deadline        string    `json:"deadline" faker:"timestamp"`
-	Comments        []Comment `json:"comments" gorm:"foreignKey:CID;constraint:OnDelete:CASCADE;"`
+	CID                uint      `json:"cid" gorm:"primaryKey"`
+	BID                uint      `json:"bid" gorm:"not null;index"`
+	CLID               uint      `json:"clid" gorm:"not null;index"`
+	PositionOnCardList uint      `json:"pos" faker:"-"`
+	Title              string    `json:"card_name" faker:"word" gorm:"not null;index"`
+	Description        string    `json:"description" faker:"sentence"`
+	DeadlineChecked    bool      `json:"deadline_check" faker:"-"`
+	Deadline           string    `json:"deadline" faker:"timestamp"`
+	Comments           []Comment `json:"comments" gorm:"foreignKey:CID;constraint:OnDelete:CASCADE;"`
 }
