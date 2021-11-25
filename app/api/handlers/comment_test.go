ec57d87d2a5ffb1c6b5c515c1bd4d111f1018355
DPeshkoff <dmytrypeshkov@gmail.com>
2021-11-25 05:40:39 +0300

    LDBR-3.12: Добавить тесты для хэндлеров комментариев.

--- app/api/handlers/comment_test.go
+++ app/api/handlers/comment_test.go
@@ -163,3 +163,87 @@ func TestGetComment(t *testing.T) {
 	
 	assert.Equal(t, http.StatusUnauthorized, w.Code)
 }
+
+func TestDeleteComment(t *testing.T) {
+	t.Parallel()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	sessionMock := mocks.NewMockSessionUseCase(ctrl)
+	useCaseMock := mocks.NewMockCommentUseCase(ctrl)
+
+	gin.SetMode(gin.ReleaseMode)
+	router := gin.Default()
+	var logger zapLogger.Logger
+	logger.InitLogger("./logs.log")
+	everythingCloser := closer.CreateCloser(&logger)
+	defer everythingCloser.Close(logger.Sync)
+	commonMW := CreateCommonMiddleware(logger)
+	router.Use(commonMW.Logger())
+	mw := CreateSessionMiddleware(sessionMock)
+	api := router.Group("/api")
+	CreateCommentHandler(api, "/comments", useCaseMock, mw)
+
+	cookie := &http.Cookie{
+		Name:  "session_id",
+		Value: "1",
+	}
+
+	csrfToken := &http.Cookie{
+		Name:  "csrf_token",
+		Value: "1",
+	}
+
+	testUID := uint(1)
+
+	testComment := new(models.Comment)
+	err := faker.FakeData(testComment)
+	testComment.UID = testUID
+	testComment.CMID = uint(1)
+	assert.NoError(t, err)
+
+	// success
+	sessionMock.EXPECT().GetUID(cookie.Value).Return(testUID, nil)
+	useCaseMock.EXPECT().DeleteComment(testUID, testComment.CMID).Return(nil)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("DELETE", "/api/comments/1", nil)
+	req.AddCookie(cookie)
+	req.AddCookie(csrfToken)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+
+	// ParseUint not parsing
+	sessionMock.EXPECT().GetUID(cookie.Value).Return(testUID, nil)
+
+	w = httptest.NewRecorder()
+	req, _ = http.NewRequest("DELETE", "/api/comments/tete", nil)
+	req.AddCookie(cookie)
+	req.AddCookie(csrfToken)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusBadRequest, w.Code)
+
+ 	// not authorized
+	sessionMock.EXPECT().GetUID(cookie.Value).Return(uint(0), customErrors.ErrNotAuthorized)
+
+	w = httptest.NewRecorder()
+	req, _ = http.NewRequest("DELETE", "/api/comments/1", nil)
+	req.AddCookie(cookie)
+	req.AddCookie(csrfToken)
+	router.ServeHTTP(w, req)
+	
+	assert.Equal(t, http.StatusUnauthorized, w.Code)
+
+	// fail
+	sessionMock.EXPECT().GetUID(cookie.Value).Return(testUID, nil)
+	useCaseMock.EXPECT().DeleteComment(testUID, testComment.CMID).Return(customErrors.ErrNoAccess)
+	
+	w = httptest.NewRecorder()
+	req, _ = http.NewRequest("DELETE", "/api/comments/1", nil)
+	req.AddCookie(cookie)
+	req.AddCookie(csrfToken)
+	router.ServeHTTP(w, req)
+	
+	assert.Equal(t, http.StatusForbidden, w.Code)
+}
