1b5cd741d326f47f58139fe2e29272096e64b55f
DPeshkoff <dmytrypeshkov@gmail.com>
2021-11-25 04:38:57 +0300

    LDBR-3.12: Написать тесты на session handler, написать тесты на create и get comment.

---
+++ app/api/handlers/comment_test.go
@@ -0,0 +1,165 @@
+package handlers
+
+import (
+	"backendServer/app/api/models"
+	"backendServer/app/api/usecases/mocks"
+	"backendServer/pkg/closer"
+	customErrors "backendServer/pkg/errors"
+	zapLogger "backendServer/pkg/logger"
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/golang/mock/gomock"
+
+	"github.com/bxcodec/faker/v3"
+	"github.com/gin-gonic/gin"
+)
+
+func TestCreateComment(t *testing.T) {
+	t.Parallel()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	sessionMock := mocks.NewMockSessionUseCase(ctrl)
+	useCaseMock := mocks.NewMockCommentUseCase(ctrl)
+
+	gin.SetMode(gin.ReleaseMode)
+	router := gin.Default()
+	var logger zapLogger.Logger
+	logger.InitLogger("./logs.log")
+	everythingCloser := closer.CreateCloser(&logger)
+	defer everythingCloser.Close(logger.Sync)
+	commonMW := CreateCommonMiddleware(logger)
+	router.Use(commonMW.Logger())
+	mw := CreateSessionMiddleware(sessionMock)
+	api := router.Group("/api")
+	CreateCommentHandler(api, "/comments", useCaseMock, mw)
+
+	cookie := &http.Cookie{
+		Name:  "session_id",
+		Value: "1",
+	}
+
+	csrfToken := &http.Cookie{
+		Name:  "csrf_token",
+		Value: "1",
+	}
+
+	testUID := uint(1)
+
+	testComment := new(models.Comment)
+	err := faker.FakeData(testComment)
+	testComment.UID = testUID
+	body, err := json.Marshal(testComment)
+	assert.NoError(t, err)
+
+	// success
+	sessionMock.EXPECT().GetUID(cookie.Value).Return(testUID, nil)
+	useCaseMock.EXPECT().CreateComment(testComment).Return(testComment, nil)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", "/api/comments", bytes.NewBuffer(body))
+	req.AddCookie(cookie)
+	req.AddCookie(csrfToken)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+
+	// json not binding
+	sessionMock.EXPECT().GetUID(cookie.Value).Return(testUID, nil)
+
+	w = httptest.NewRecorder()
+	req, _ = http.NewRequest("POST", "/api/comments", bytes.NewBuffer(body[:1]))
+	req.AddCookie(cookie)
+	req.AddCookie(csrfToken)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusBadRequest, w.Code)
+
+	// not authorized
+	sessionMock.EXPECT().GetUID(cookie.Value).Return(uint(0), customErrors.ErrNotAuthorized)
+
+	w = httptest.NewRecorder()
+	req, _ = http.NewRequest("POST", "/api/comments", bytes.NewBuffer(body))
+	req.AddCookie(cookie)
+	req.AddCookie(csrfToken)
+	router.ServeHTTP(w, req)
+	
+	assert.Equal(t, http.StatusUnauthorized, w.Code)
+}
+
+func TestGetComment(t *testing.T) {
+	t.Parallel()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	sessionMock := mocks.NewMockSessionUseCase(ctrl)
+	useCaseMock := mocks.NewMockCommentUseCase(ctrl)
+
+	gin.SetMode(gin.ReleaseMode)
+	router := gin.Default()
+	var logger zapLogger.Logger
+	logger.InitLogger("./logs.log")
+	everythingCloser := closer.CreateCloser(&logger)
+	defer everythingCloser.Close(logger.Sync)
+	commonMW := CreateCommonMiddleware(logger)
+	router.Use(commonMW.Logger())
+	mw := CreateSessionMiddleware(sessionMock)
+	api := router.Group("/api")
+	CreateCommentHandler(api, "/comments", useCaseMock, mw)
+
+	cookie := &http.Cookie{
+		Name:  "session_id",
+		Value: "1",
+	}
+
+	csrfToken := &http.Cookie{
+		Name:  "csrf_token",
+		Value: "1",
+	}
+
+	testUID := uint(1)
+
+	testComment := new(models.Comment)
+	err := faker.FakeData(testComment)
+	testComment.UID = testUID
+	testComment.CMID = uint(1)
+	assert.NoError(t, err)
+
+	// success
+	sessionMock.EXPECT().GetUID(cookie.Value).Return(testUID, nil)
+	useCaseMock.EXPECT().GetComment(testUID, testComment.CMID).Return(testComment, nil)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/api/comments/1", nil)
+	req.AddCookie(cookie)
+	req.AddCookie(csrfToken)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+
+	// ParseUint not parsing
+	sessionMock.EXPECT().GetUID(cookie.Value).Return(testUID, nil)
+
+	w = httptest.NewRecorder()
+	req, _ = http.NewRequest("GET", "/api/comments/tete", nil)
+	req.AddCookie(cookie)
+	req.AddCookie(csrfToken)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusBadRequest, w.Code)
+
+ 	// not authorized
+	sessionMock.EXPECT().GetUID(cookie.Value).Return(uint(0), customErrors.ErrNotAuthorized)
+
+	w = httptest.NewRecorder()
+	req, _ = http.NewRequest("GET", "/api/comments/1", nil)
+	req.AddCookie(cookie)
+	req.AddCookie(csrfToken)
+	router.ServeHTTP(w, req)
+	
+	assert.Equal(t, http.StatusUnauthorized, w.Code)
+}
--- app/api/handlers/session_test.go
+++ app/api/handlers/session_test.go
@@ -43,11 +43,9 @@ func TestCreateSession(t *testing.T) {
 	assert.NoError(t, err)
 
 	body, err := json.Marshal(testUser)
-	assert.NoError(t, err)
 
 	// success
 	useCaseMock.EXPECT().Create(testUser).Return("testSid", nil)
-	assert.NoError(t, err)
 
 	w := httptest.NewRecorder()
 	req, _ := http.NewRequest("POST", "/api/sessions", bytes.NewBuffer(body))
@@ -64,10 +62,136 @@ func TestCreateSession(t *testing.T) {
 
 	// not authorized
 	useCaseMock.EXPECT().Create(testUser).Return("", customErrors.ErrUserNotFound)
-	assert.NoError(t, err)
 
 	w = httptest.NewRecorder()
 	req, _ = http.NewRequest("POST", "/api/sessions", bytes.NewBuffer(body))
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusUnauthorized, w.Code)
+}
+
+func TestGetSession(t *testing.T) {
+	t.Parallel()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	useCaseMock := mocks.NewMockSessionUseCase(ctrl)
+
+	gin.SetMode(gin.ReleaseMode)
+	router := gin.Default()
+	var logger zapLogger.Logger
+	logger.InitLogger("./logs.log")
+	everythingCloser := closer.CreateCloser(&logger)
+	defer everythingCloser.Close(logger.Sync)
+
+	commonMW := CreateCommonMiddleware(logger)
+	router.Use(commonMW.Logger())
+	mw := CreateSessionMiddleware(useCaseMock)
+	api := router.Group("/api")
+	CreateSessionHandler(api, "/sessions", useCaseMock, mw)
+
+	cookie := &http.Cookie{
+		Name:  "session_id",
+		Value: "1",
+	}
+
+	testUID := uint(1)
+	testLogin := "login"
+
+	// success
+	useCaseMock.EXPECT().GetUID(cookie.Value).Return(testUID, nil)
+	useCaseMock.EXPECT().Get(cookie.Value).Return(testLogin, nil)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/api/sessions", nil)
+	req.AddCookie(cookie)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+
+	// not authorized
+	useCaseMock.EXPECT().GetUID(cookie.Value).Return(testUID, nil)
+	useCaseMock.EXPECT().Get(cookie.Value).Return("", customErrors.ErrNotAuthorized)
+
+	w = httptest.NewRecorder()
+	req, _ = http.NewRequest("GET", "/api/sessions", nil)
+	req.AddCookie(cookie)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusUnauthorized, w.Code)
+
+	// session not found
+	useCaseMock.EXPECT().GetUID(cookie.Value).Return(uint(0), customErrors.ErrNotAuthorized)
+
+	w = httptest.NewRecorder()
+	req, _ = http.NewRequest("GET", "/api/sessions", nil)
+	req.AddCookie(cookie)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusUnauthorized, w.Code)
+}
+
+func TestDeleteSession(t *testing.T) {
+	t.Parallel()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	useCaseMock := mocks.NewMockSessionUseCase(ctrl)
+
+	gin.SetMode(gin.ReleaseMode)
+	router := gin.Default()
+	var logger zapLogger.Logger
+	logger.InitLogger("./logs.log")
+	everythingCloser := closer.CreateCloser(&logger)
+	defer everythingCloser.Close(logger.Sync)
+
+	commonMW := CreateCommonMiddleware(logger)
+	router.Use(commonMW.Logger())
+	mw := CreateSessionMiddleware(useCaseMock)
+	api := router.Group("/api")
+	CreateSessionHandler(api, "/sessions", useCaseMock, mw)
+
+	cookie := &http.Cookie{
+		Name:  "session_id",
+		Value: "1",
+	}
+
+	csrfToken := &http.Cookie{
+		Name:  "csrf_token",
+		Value: "1",
+	}
+
+	testUID := uint(1)
+
+	// success
+	useCaseMock.EXPECT().GetUID(cookie.Value).Return(testUID, nil)
+	useCaseMock.EXPECT().Delete(cookie.Value).Return(nil)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("DELETE", "/api/sessions", nil)
+	req.AddCookie(cookie)
+	req.AddCookie(csrfToken)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+
+	// not authorized
+	useCaseMock.EXPECT().GetUID(cookie.Value).Return(testUID, nil)
+	useCaseMock.EXPECT().Delete(cookie.Value).Return(customErrors.ErrNotAuthorized)
+
+	w = httptest.NewRecorder()
+	req, _ = http.NewRequest("DELETE", "/api/sessions", nil)
+	req.AddCookie(cookie)
+	req.AddCookie(csrfToken)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusUnauthorized, w.Code)
+
+	// session not found
+	useCaseMock.EXPECT().GetUID(cookie.Value).Return(uint(0), customErrors.ErrNotAuthorized)
+
+	w = httptest.NewRecorder()
+	req, _ = http.NewRequest("DELETE", "/api/sessions", nil)
+	req.AddCookie(cookie)
+	req.AddCookie(csrfToken)
 	router.ServeHTTP(w, req)
 
 	assert.Equal(t, http.StatusUnauthorized, w.Code)
